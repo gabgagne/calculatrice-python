5e0246590c1e4f192e2eb39a63d918502176f712
gabgagne <gabevangagne@gmail.com>
2022-02-08 14:02:33 -0500

    Calculatrice

---
+++ Calculatrice.py
@@ -0,0 +1,21 @@
+z = str(input('+, - ou x : '))
+choix_de_symbole = f'{z}'
+
+
+
+        
+
+
+
+a = int(input('Entrez un premier nombre : '))
+b = int(input('Entrez un deuxième nombre : '))
+resultat = f'Le résulat de {a} + {b} est de {a + b}'
+print(resultat)
+c = int(input('Entrez un premier nombre : '))
+d = int(input('Entrez un deuxième nombre : '))
+produit = f'Le résultat de {c} x {d} est de {c * d}'
+print(produit)
+e = int(input('Entrez un premier nombre : '))
+f = int(input('Entrez un deuxième nombre : '))
+différence = f'Le résultat de {e} - {f} est de {e - f}'
+print(différence)
